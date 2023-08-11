b44f71edec25e76e9c15e5c33599ef695e7fdeea
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-08-11 18:47:01 +0100

    running now

--- login.py
+++ login.py
@@ -9,23 +9,19 @@ def loginUser():
       if(request.method =="POST"):
             var1 = request.form["n1"]
             var2 = request.form["n2"]
-            # reader = open("UserListDB.txt", "r")
+            reader = open("UserListDB.txt", "r")
 
-            # v =reader.readline().strip()
-            # log =0
-            return render_template("results.html", res = str(request.method + " if"))
-            # while(v!=""):
-            #       l = v.split(" ")
-            #       if(var1==l[0] and var2 ==l[1]):
-            #             log =1
-            #       v = reader.readline().strip()
-            # if(log ==1):
-            # return render_template("results.html", res = log+ " logged sucessfuly ") 
-            # else:
-            #       return render_template("results.html", res = "bad credentials")
-
+            v =reader.readline().strip()
+            log =0
+            while(v!=""):
+                  l = v.split(" ")
+                  if(var1==l[0] and var2 ==l[1]):
+                        log =1
+                  v = reader.readline().strip()
+            
+            return render_template("results.html", res = var1 + " logged ")
       else:
-            return render_template("results.html", res = str(request.method + " else"))
+            return render_template("input.html", res =request.method)
             
 
 
